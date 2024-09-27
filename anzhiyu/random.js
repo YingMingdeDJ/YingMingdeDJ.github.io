28d3dc08005360f1ae1fcf06744e6cb68e5a2d5a
zhuji <zhuji>
2024-09-27 23:45:49 +0800

    Site updated: 2024-09-27 23:45:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/25/hello-world/","2024/09/26/Linux的使用/"];function toRandomPost(){
+var posts=["2024/09/25/hello-world/","2024/09/27/和DC斗智斗勇/","2024/09/27/调整Linux环境/","2024/09/27/为什么寄存器需要建立时间和保持时间约束？——从门级角度理解/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
